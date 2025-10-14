bb3b544732f8d0dd55cd15b8a8b91bcf10fa304c
aryapromotora-alt <eduardomurtada@gmail.com>
2025-10-13 23:55:33 -0300

    Add files via upload

--- models/user.py
+++ models/user.py
@@ -4,25 +4,24 @@ from werkzeug.security import generate_password_hash, check_password_hash
 db = SQLAlchemy()
 
 class User(db.Model):
+    __tablename__ = 'users'
+
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(128), nullable=False)
-    role = db.Column(db.String(20), default="user")  # "user" ou "admin"
-
-    def __repr__(self):
-        return f'<User {self.username}>'
+    email = db.Column(db.String(120), unique=True, nullable=True)
+    password_hash = db.Column(db.String(128), nullable=False)
+    role = db.Column(db.String(20), default="user")
 
     def set_password(self, password):
-        self.password = generate_password_hash(password)
+        self.password_hash = generate_password_hash(password)
 
     def check_password(self, password):
-        return check_password_hash(self.password, password)
+        return check_password_hash(self.password_hash, password)
 
     def to_dict(self):
         return {
-            'id': self.id,
-            'username': self.username,
-            'email': self.email,
-            'role': self.role
+            "id": self.id,
+            "username": self.username,
+            "email": self.email,
+            "role": self.role
         }
